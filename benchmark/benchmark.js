1910470e5d67014151d7f94d213ff5932cfb3996
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-07-20 03:11:01 +0200

    Repair benchmark script and exclude fixtures from npm package

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,48 +1,40 @@
-var GrammarRegistry, cssGrammar, fs, jsGrammar, path, registry, tokenize, tokenizeFile;
+const path = require('path');
+const fs = require('fs');
+const { GrammarRegistry, ready } = require('../lib/second-mate.js');
 
-path = require('path');
-
-fs = require('@lumine-code/fs-plus');
-
-GrammarRegistry = require('../src/grammar-registry');
-
-registry = new GrammarRegistry();
-
-jsGrammar = registry.loadGrammarSync(path.resolve(__dirname, '..', 'spec', 'fixtures', 'javascript.json'));
-
-jsGrammar.maxTokensPerLine = 2e308;
-
-cssGrammar = registry.loadGrammarSync(path.resolve(__dirname, '..', 'spec', 'fixtures', 'css.cson'));
-
-cssGrammar.maxTokensPerLine = 2e308;
-
-tokenize = function(grammar, content, lineCount) {
-  var duration, i, len, start, tokenCount, tokenizedLine, tokenizedLines, tokensPerMillisecond;
-  start = Date.now();
-  tokenizedLines = grammar.tokenizeLines(content, false);
-  duration = Date.now() - start;
-  tokenCount = 0;
-  for (i = 0, len = tokenizedLines.length; i < len; i++) {
-    tokenizedLine = tokenizedLines[i];
+function tokenize(grammar, content, lineCount) {
+  const start = Date.now();
+  const tokenizedLines = grammar.tokenizeLines(content, false);
+  const duration = Date.now() - start;
+  let tokenCount = 0;
+  for (const tokenizedLine of tokenizedLines) {
     tokenCount += tokenizedLine.length;
   }
-  tokensPerMillisecond = Math.round(tokenCount / duration);
-  return console.log("Generated " + tokenCount + " tokens for " + lineCount + " lines in " + duration + "ms (" + tokensPerMillisecond + " tokens/ms)");
-};
+  const tokensPerMillisecond = Math.round(tokenCount / duration);
+  console.log(`Generated ${tokenCount} tokens for ${lineCount} lines in ${duration}ms (${tokensPerMillisecond} tokens/ms)`);
+}
 
-tokenizeFile = function(filePath, grammar, message) {
-  var content, lineCount;
+function tokenizeFile(filePath, grammar, message) {
   console.log();
   console.log(message);
-  content = fs.readFileSync(filePath, 'utf8');
-  lineCount = content.split('\n').length;
-  return tokenize(grammar, content, lineCount);
-};
+  const content = fs.readFileSync(filePath, 'utf8');
+  const lineCount = content.split('\n').length;
+  tokenize(grammar, content, lineCount);
+}
 
-tokenizeFile(path.join(__dirname, 'large.js'), jsGrammar, 'Tokenizing jQuery v2.0.3');
+async function main() {
+  await ready;
 
-tokenizeFile(path.join(__dirname, 'large.min.js'), jsGrammar, 'Tokenizing jQuery v2.0.3 minified');
+  const registry = new GrammarRegistry();
+  const jsGrammar = registry.loadGrammarSync(path.resolve(__dirname, '..', 'spec', 'fixtures', 'javascript.json'));
+  jsGrammar.maxTokensPerLine = Infinity;
+  const cssGrammar = registry.loadGrammarSync(path.resolve(__dirname, '..', 'spec', 'fixtures', 'css.cson'));
+  cssGrammar.maxTokensPerLine = Infinity;
 
-tokenizeFile(path.join(__dirname, 'bootstrap.css'), cssGrammar, 'Tokenizing Bootstrap CSS v3.1.1');
+  tokenizeFile(path.join(__dirname, 'large.js'), jsGrammar, 'Tokenizing jQuery v2.0.3');
+  tokenizeFile(path.join(__dirname, 'large.min.js'), jsGrammar, 'Tokenizing jQuery v2.0.3 minified');
+  tokenizeFile(path.join(__dirname, 'bootstrap.css'), cssGrammar, 'Tokenizing Bootstrap CSS v3.1.1');
+  tokenizeFile(path.join(__dirname, 'bootstrap.min.css'), cssGrammar, 'Tokenizing Bootstrap CSS v3.1.1 minified');
+}
 
-tokenizeFile(path.join(__dirname, 'bootstrap.min.css'), cssGrammar, 'Tokenizing Bootstrap CSS v3.1.1 minified');
+main();
